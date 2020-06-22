66f95d32befef7b7b613f66ffa5e383cedc66a96
yangtzesyy <yangtzesyy@gmail.com>
2020-06-22 21:09:21 +0800

    hahahah

---
+++ Untitled.R
@@ -0,0 +1,8 @@
+#在R官网下载最新的R，把老版本的包移动到新的地方，运行下面这行代码
+update.packages(checkBuilt = TRUE, ask = FALSE)
+
+library(ggplot2)
+library(knitr)
+library(foreign)        
+library(markdown)
+library(tinytex)
